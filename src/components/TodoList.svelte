b90b82f8008ac0b439f433ddb87c7db60f9579ca
Nick Dubelman <ndubelman@arista.com>
2021-02-10 09:03:56 -0800

    show todos by newest first. add button for clearing completed todos

--- src/components/TodoList.svelte
+++ src/components/TodoList.svelte
@@ -2,16 +2,25 @@
   import type { Todo } from '../entities'
 
   export let todos: Todo[]
+  $: orderedTodos = [...todos].reverse()
+
+  const clearCompleted = () => {
+    todos = todos.filter(t => !t.completed)
+  }
 </script>
 
 <ul>
-  {#each todos as todo}
+  {#each orderedTodos as todo}
     <li>
       <input type="checkbox" bind:checked={todo.completed} />
       {todo.title}
     </li>
   {/each}
 </ul>
+
+{#if todos.filter(t => t.completed).length > 0}
+  <button on:click={clearCompleted}>Clear completed</button>
+{/if}
 
 <style>
   ul {
